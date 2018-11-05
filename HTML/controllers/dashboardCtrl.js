935b9a181affc3455040cdb2bbf494f09327e916
Munchlaxx <evan.mirschel@gmail.com>
2018-11-05 16:55:55 -0500

    Initial commit

---
+++ HTML/controllers/dashboardCtrl.js
@@ -0,0 +1,218 @@
+angular.module('dbApp', ['ngMaterial']).controller('DashboardCtrl', function($scope, $mdDialog, $http) {
+    $scope.createEventToggle = false;
+    $scope.createRSOToggle = false;
+    $scope.showEventSearchResults = false;
+    $scope.showRSOSearchResults = false;
+    $scope.viewEventToggle = false;
+    //userInfo - holds user data from sign in
+    $scope.userInfo;
+    //creating new event
+    $scope.event = {};
+    $scope.event.name = '';
+    $scope.event.startTime = '';
+    $scope.event.endTime = '';
+    $scope.event.description = '';
+    $scope.event.cat = 0;
+    $scope.event.location = 0;
+    $scope.event.userID = 0;
+    //creating new RSO
+    $scope.rso = {};
+    $scope.rso.name = '';
+    $scope.rso.universityID = 1;
+    $scope.rso.active = 0;
+    $scope.rso.userID = 2;
+    $scope.addMember = '';
+    $scope.foundingMembers = [];
+    //searching
+    $scope.searchResults = [];
+    $scope.response = '';
+    $scope.searchParam = '';
+    $scope.searchCat = '';
+    $scope.userData = {};
+    $scope.userData.userID = '11223';
+    $scope.userData.admin = true;
+
+    $scope.rsoArrayPush = function(){
+        $scope.foundingMembers.push($scope.addMember);
+        $scope.addMember = '';
+    }
+    $scope.search = function(){
+        //package for db
+        $scope.createEventToggle = false;
+        $scope.createRSOToggle = false;
+        if($scope.searchCat == '0'){
+            //public event
+            var dataToSend = {};
+            dataToSend.name = $scope.searchParam;
+            dataToSend.cat = 0;
+            $scope.json = angular.toJson(dataToSend);
+            $http.post('/searchEvents', $scope.json).then(function(data){
+                $scope.response = data;
+                console.log("retrieved data: ");
+                console.log($scope.response);
+                $scope.searchResults = angular.fromJson($scope.response);
+                console.log("Parsed response: ");
+                console.log($scope.searchResults);
+                $scope.showEventSearchResults = true;
+            });        
+            
+        }
+        if($scope.searchCat == '1'){
+            //private event
+            var dataToSend = {};
+            dataToSend.name = $scope.searchParam;
+            dataToSend.cat = 1;
+            $scope.json = angular.toJson(dataToSend);
+            $http.post('/searchEvents', $scope.json).then(function(data){
+                $scope.response = data;
+                console.log("retrieved data: ");
+                console.log($scope.response);
+                $scope.searchResults = angular.fromJson($scope.response);
+                console.log("Parsed response: ");
+                console.log($scope.searchResults);
+                $scope.showEventSearchResults = true;
+            });
+        }
+        if($scope.searchCat == '2'){
+            //rso event
+            var dataToSend = {};
+            dataToSend.name = $scope.searchParam;
+            dataToSend.cat = 2;
+            $scope.json = angular.toJson(dataToSend);
+            $http.post('/searchEvents', $scope.json).then(function(data){
+                $scope.response = data;
+                console.log("retrieved data: ");
+                console.log($scope.response);
+                $scope.searchResults = angular.fromJson($scope.response);
+                console.log("Parsed response: ");
+                console.log($scope.searchResults);
+                $scope.showEventSearchResults = true;
+            });
+        }
+        if($scope.searchCat == '3'){
+            //rso org
+            var dataToSend = {};
+            dataToSend.name = $scope.searchParam;
+            $scope.json = angular.toJson(dataToSend);
+            $http.post('/searchRSO', $scope.json).then(function(data){
+                $scope.response = data;
+                console.log("retrieved data: ");
+                console.log($scope.response);
+                $scope.searchResults = angular.fromJson($scope.response);
+                console.log("Parsed response: ");
+                console.log($scope.searchResults);
+                $scope.showRSOSearchResults = true;
+            });
+          
+        }
+    }
+    $scope.viewEvent = function(event){
+
+    }
+
+    $scope.joinRSO = function(rso){
+        console.log(rso);
+        $scope.dataToSend = {};
+        $scope.dataToSend.name = rso.name;
+        $scope.dataToSend.attendee; //code to get userID here
+        $scope.json = angular.toJson(dataToSend);
+    }
+
+    $scope.attendEvent = function(event){
+        console.log(event);
+        $scope.dataToSend = {};
+        $scope.dataToSend.name = event.name;
+        $scope.dataToSend.attendee = $scope.userInfo.userID; 
+        $scope.json = angular.toJson($scope.dataToSend);
+        $http.post('/attendEvent', $scope.json).then(function(data){
+            alert("Successfully Attending Event!");
+        },
+        function(data){
+            alert("Failed to Enroll");
+        });
+    $scope.createEvent = function(){
+        //gather info and send to db
+        $scope.createEventToggle = true;
+        $scope.createRSOToggle = false;
+        if($scope.event.name != ''){
+            $scope.json = angular.toJson($scope.event);
+            $http.post('/createEvent', $scope.json);
+        }
+    }
+}
+    $scope.createRSO = function(){
+        //form with 5 users
+        $scope.createRSOToggle = true;
+        $scope.createEventToggle = false;
+        if($scope.rso.name != ''){
+            $scope.json = angular.toJson($scope.rso);
+            console.log($scope.json);
+            $http.post('/createRSO', $scope.json);
+        }
+    }
+    $scope.goto = function(key){
+        if(key == 'search'){
+            window.location.href="searchEngine.html"
+        }
+        if(key == 'myEvents'){
+            window.location.href="myEvents.html"
+        }
+        if(key == 'myRSOs'){
+            window.location.href="myRSOs.html"
+        }
+    }
+    $scope.logout = function(){
+        //clear out user info 
+        window.location.href="signIn.html"
+    }
+
+    //login ctrl
+     //login data
+     $scope.userData = {};
+     $scope.userData.email = '';
+     $scope.userData.password = '';
+ 
+     //sign up data
+     $scope.universityList = [{name: 'UCF', code: '1'}, {name: 'Valencia', code: '2'}]; //to be replaced later when DB call works
+     $scope.securityQList = ["What is your favorite animal?",
+                             "What elementary school did you attend?",
+                             "What is your least favorite food?"
+                             ];
+     $scope.securityQSelected = '';
+     $scope.signUpData = {};
+     $scope.signUpData.email = '';
+     $scope.signUpData.password = '';
+     $scope.signUpData.universityID = 1;
+     $scope.signUpData.securityAns = '';
+     $scope.signUpData.isAdmin = 0;
+     $scope.signUpData.isSuperAdmin = 0;
+     $scope.signUpToggle = false;
+ 
+ 
+     $scope.login = function(){
+         //send userData to database, if valid user, get permissions and go to next page
+         $scope.json = angular.toJson($scope.userData);
+         $http.post('/userLogin', $scope.json).then(function(data){
+            $scope.userInfo = angular.fromJson(data);
+            window.location.href="dashboard.html"
+            console.log("login success");
+        },
+        function(data){
+            //incorrect user info
+            console.log("login failed");
+            alert("Incorrect Login");
+        });
+     }
+     $scope.signUpFunc = function(){
+         $scope.signUpToggle = true;
+         if($scope.signUpData.uni != 0 && $scope.signUpData.email != '' && $scope.signUpData.password != ''){
+             //submit new user to db here
+             $scope.json = angular.toJson($scope.signUpData);
+             $http.post('/addUser', $scope.json).then(function(data){
+                $scope.userInfo = angular.fromJson(data);
+                window.location.href="dashboard.html"
+                console.log("login success");
+            });
+     }
+    }
+});
---
+++ app.js
@@ -0,0 +1,383 @@
+// Library imports
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const PORT = 3000
+var path = require('path');
+
+// Creating the express server
+const app = express();
+
+// Starting the express server
+app.listen(PORT, function()
+{
+	console.log("Listening on " + PORT)
+});
+
+// Creating the MySQL connection
+var db = mysql.createPool({
+	connectionLimit: 10,
+	host     : 'cop4710db.cl3pgb8hw8fv.us-east-1.rds.amazonaws.com',
+  	user     : 'cop4710user',
+  	password : 'cop4710pass',
+  	database : 'akean_project'
+
+});
+
+// Body-parser initialization
+app.set('trust proxy', 1);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+	secret: '7i5mnQZjPSqL924rQvxG',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		secure: true,
+		maxAge: 86400000
+	}
+}));
+
+// Sets directory for files to serve (files not in this directory will not be served)
+app.use(express.static(path.join(__dirname, 'HTML')));
+
+// 6 pages total
+// Pulling the login page
+app.get('/home',function(req,res){
+    res.sendFile(__dirname + '/html/dashboard.html');
+});
+
+app.get('/login',function(req,res){
+    res.sendFile(__dirname + '/html/signin.html');
+});
+
+//////////////////////////////////////////////////////////////////////
+
+
+//Routes
+
+//Create user
+app.post('/addUser', function(req, res){
+    // Create connection to database
+	db.getConnection(function(err, tempCont){
+			
+		// Error if connection is not established
+		if(err) {
+			res.status(400).send('Connetion Fail');				
+		} 
+				
+		else {
+			// Add user to database
+			const sqlAddUser = "INSERT INTO STD_USER (password, email, universityID, securityAns) VALUES ('";
+			tempCont.query(sqlAddUser + req.body.password + "', '" + req.body.email + "', " + req.body.universityID + ", '" + req.body.securityAns + "')", function(err, result) {
+
+			// Check if query works
+			if(err) {
+				res.status(400).send('Query Fail');
+			}
+			else {
+				res.status(200).send('Query Success');	
+			}
+								
+			// End connection
+			tempCont.release();
+			});
+								
+		}
+    });
+});
+
+//User login
+app.post('/userLogin', function(req,res){
+    // Create connection to database
+	db.getConnection(function(err, tempCont){
+			
+		// Error if connection is not established
+		if(err) {
+			res.status(400).send('Connection fail');				
+		} 
+		
+		else { 			
+			const sql = 'SELECT userID, email, universityID FROM STD_USER WHERE email = ? AND password = ?';
+			tempCont.query(sql, [req.body.email, req.body.password], function(err, result) {
+					
+				// Check if query works
+				if (err) {
+					res.status(400).send('Query Fail');
+				}
+				 
+                else {
+					if (result.length == 0){
+						res.status(400).send('No match')
+					}
+					else{
+						res.status(200).send(result);	
+					}
+							
+				}
+					
+				// End connection
+				tempCont.release();
+			
+			});
+				
+		}
+
+	});
+});
+
+
+
+
+// Search for events.
+app.post('/searchEvents', function(req,res){
+    // Create connection to database
+	db.getConnection(function(err, tempCont){
+			
+		// Error if connection is not established
+		if(err) {
+			res.status(400).send('Connection fail');
+				
+		} else { 
+			
+			
+			if(req.body.flag == 0){
+				const sqlSearchEvent = 'SELECT * FROM ALL_EVENTS WHERE name = ? AND cat = ?';
+				tempCont.query(sqlSearchEvent,[req.body.name, req.body.cat], function(err, result) {
+					
+				// Check if query works
+				if (err) {
+					res.status(400).send('Query Fail');
+                } 
+                else {
+					res.status(200).send(result);		
+				}
+					
+				// End connection
+				tempCont.release();
+			
+				});
+			}
+			else{
+				const sqlSearchEvent = 'SELECT * FROM ALL_EVENTS WHERE cat = ?';
+				tempCont.query(sqlSearchEvent,[req.body.cat], function(err, result) {
+					
+				// Check if query works
+				if (err) {
+					res.status(400).send('Query Fail');
+                } 
+                else {
+					res.status(200).send(result);		
+				}
+					
+				// End connection
+				tempCont.release();
+			
+				});
+			}
+				
+		}
+
+	});
+});
+
+
+// Create Event.
+app.post('/createEvent', function(req,res){
+
+    // Create connection to database
+	db.getConnection(function(err, tempCont){
+			
+		// Error if connection is not established
+		if(err) {
+			res.status(400).send('Connection fail');
+				
+		} else { 
+			
+			
+			const sqlCreateEvent = 'INSERT INTO ALL_EVENTS (userID, cat, startTime, endTime, location, name, description) VALUES(';
+			tempCont.query(sqlCreateEvent + req.body.userID + "," + req.body.cat + ", '" + req.body.startTime + "', '" + req.body.endTime + "'," + req.body.location + ", '" + req.body.name + "', '" + req.body.description + "')", function(err, result) {
+					
+				// Check if query works
+				if (err) {
+					res.status(400).send('Query Fail');
+                } 
+                else {
+					res.status(200).send(result);		
+				}
+					
+				// End connection
+				tempCont.release();
+			
+			});
+				
+		}
+
+	});
+});
+
+
+// Create RSO.
+app.post('/createRSO', function(req,res){
+
+    // Create connection to database
+	db.getConnection(function(err, tempCont){
+			
+		// Error if connection is not established
+		if(err) {
+			res.status(400).send('Connection fail');
+				
+		} else { 
+			
+			
+			const sqlCreateEvent = 'INSERT INTO RSO (universityID, userID, name) VALUES(';
+			tempCont.query(sqlCreateEvent + req.body.universityID + "," + req.body.userID + ", '" + req.body.name + "')", function(err, result) {
+					
+				// Check if query works
+				if (err) {
+					res.status(400).send('Query Fail');
+                } 
+                else {
+					res.status(200).send(result);		
+				}
+					
+				// End connection
+				tempCont.release();
+			
+			});
+				
+		}
+
+	});
+});
+
+// Search for RSO.
+app.post('/searchRSO', function(req,res){
+
+    // Create connection to database
+	db.getConnection(function(err, tempCont){
+			
+		// Error if connection is not established
+		if(err) {
+			res.status(400).send('Connection fail');
+				
+		} else { 
+			
+			
+			const sqlSearchRSO = 'SELECT * FROM RSO WHERE name = ?';
+			tempCont.query(sqlSearchRSO,[req.body.name], function(err, result) {
+					
+				// Check if query works
+				if (err) {
+					res.status(400).send('Query Fail');
+                } 
+                else {
+					res.status(200).send(result);		
+				}
+					
+				// End connection
+				tempCont.release();
+			
+			});
+				
+		}
+
+	});
+});
+
+
+
+
+//Check for valid inputs to stop SQL injections.
+var checkInput = function(input, type, callback) {
+	
+	var returnVal = null;
+	
+	switch(type) {
+		
+		case "username":
+			var re = /^[a-z|\d]{1,20}$/i; // Format 5-20 characters and digit
+			returnVal = re.test(input);
+			break;
+
+		case "password":
+			 var re= /[a-z\d]{32}$/;
+			 returnVal= re.test(input);
+			 break;
+			
+		case "email":
+			var re = /^[a-z\d]{1,20}@[a-z]{1,10}(\.[a-z]{3}){1,2}$/i; // Format 1-20 character @ 1-10 characters . extension
+			returnVal = re.test(input);
+			break;
+
+        // needs work
+		case "answer":
+			var re = /^[a-z|\d]{1,30}$/i;
+			var ans = input.replace(/\s/g,'');
+			ans.toLowerase();
+			console.log("The answer is:" + ans); 
+			returnVal = re.test(ans);
+			break;
+			
+		case "name":
+			var re = /^[a-z]{1,20}$/i; // Format 20 characters
+			returnVal = re.test(input);
+			break;
+			
+		case "phone":
+			var re = /(1){0,1}\d{10}$/i; // Format 18004445555 | 4074445555
+			var number = input.replace(/[^\d]/g, '');
+			returnVal = re.test(number);
+			break;
+
+		case "phonesearch":
+			var re = /\d{1,11}$/;
+			returnVal = re.test(input);
+			break;
+		
+		default:
+			returnVal = null;
+			break;
+	}
+	
+	if(callback == undefined) {	
+		return returnVal;
+		
+	} else {
+		callback(returnVal);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
